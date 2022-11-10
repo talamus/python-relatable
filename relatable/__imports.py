41f0a9579aa7ddbb0cc92a6586b9647ca3d29dbb
Tero Niemi <talamus@gmail.com>
2022-11-10 21:12:04 +0200

    Added dev and test groups

--- relatable/__imports.py
+++ relatable/__imports.py
@@ -1,6 +1,5 @@
 from collections.abc import MutableMapping, MutableSequence
-from typing import (TYPE_CHECKING, Any, Callable, Iterator, Mapping, Sequence,
-                    TypeAlias)
+from typing import TYPE_CHECKING, Any, Callable, Iterator, Mapping, Sequence, TypeAlias
 
 column_name: TypeAlias = str
 column_value: TypeAlias = Any
